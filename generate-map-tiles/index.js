daf62a77529544b60df3e0989af03f355adbf50c
Bran van der Meer <branmovic@gmail.com>
2018-05-15 18:43:23 +0200

    refactor(generate-map-tiles): Now scales to higher resolution source

--- generate-map-tiles/index.js
+++ generate-map-tiles/index.js
@@ -4,13 +4,13 @@ const gm = require('gm')
 /**
  * App: generate map tiles
  */
-const app = argv => {
+const app = (argv, tilesize) => {
   const { zoom, source, dest } = getArgs(argv)
   Array(zoom)
     .fill()
     .reduce(
       (chain, _, zoom) =>
-        chain.then(() => createZoomlevel(dest, source, 256, zoom)),
+        chain.then(() => createZoomlevel(dest, source, tilesize, zoom)),
       Promise.resolve()
     )
 }
@@ -31,68 +31,56 @@ const getArgs = argv => {
  * Create a new set of tiles
  */
 const createZoomlevel = (dir, source, tilesize, zoom) => {
-  process.stdout.write(
-    `Creating zoom level ${zoom} (${Math.pow(Math.pow(2, zoom), 2)} tiles)... `
+  console.log(
+    `\nCreating zoom level ${zoom} (${Math.pow(Math.pow(2, zoom), 2)} tiles)...`
   )
 
   const dest = `${dir}/${zoom}`
-  shell.mkdir('-p', dest)
+  const canvassize = Math.pow(2, zoom) * tilesize
 
-  const size = Math.pow(2, zoom) * tilesize
-
-  return resizeSource(source, dest, size)
-    .then(source => createTiles(source, dest, tilesize))
-    .then(() => structureXyzTiles(dest, zoom))
-    .then(() => process.stdout.write('done\n'))
+  return createTiles(source, dest, zoom, canvassize, tilesize)
 }
 
 /**
- * Resize source image to required zoomlevel
- * GraphicsMagick command:
- *  gm resize source.png 256x256 zoom.png
+ * Generate all tiles for a zoom level
  */
-const resizeSource = (source, dest, size) => {
-  const file = `${dest}/zoom.png`
-  return new Promise((resolve, reject) => {
-    gm(source)
-      .resize(size, size)
-      .write(file, err => (err ? reject(err) : resolve(file)))
-  })
-}
-
-/**
- * Generate tiles by size
- * GraphicsMagick command:
- *  gm convert source.png -crop 256x256 +adjoin %d.png
- */
-const createTiles = (source, dest, size) => {
-  return new Promise((resolve, reject) => {
-    gm(source)
-      .out('-crop', `${size}x${size}`)
-      .out('+adjoin')
-      .write(`${dest}/%d.png`, err => (err ? reject(err) : resolve()))
-  })
-}
-
-/**
- * Rename & move tiles into XYZ folder structure:
- *  dest/{zoom}/{y}/{x}
- */
-const structureXyzTiles = (dest, zoom) => {
-  const dimension = Math.pow(2, zoom)
-
-  for (let y = 0; y < dimension; y++) {
-    for (let x = 0; x < dimension; x++) {
-      const src = `${dest}/${dimension * y + x}.png`
-      shell.mkdir('-p', `${dest}/${x}`)
-      shell.mv(src, `${dest}/${y}/${x}.png`)
+const createTiles = (source, dest, zoom, canvassize, tilesize) => {
+  const tasks = []
+  const tiles = Math.pow(2, zoom)
+  for (let y = 0; y < tiles; y++) {
+    for (let x = 0; x < tiles; x++) {
+      tasks.push({ x, y })
     }
   }
 
-  return Promise.resolve()
+  // Reduce tasks to a Promise chain
+  return tasks.reduce(
+    (chain, { x, y }) =>
+      chain.then(() => createTile(source, dest, canvassize, tilesize, x, y)),
+    Promise.resolve()
+  )
+}
+
+/**
+ * Generate a single tile
+ * GraphicsMagick command:
+ *  gm convert in.png -resize 4096x4096 -crop 256x256+0+0 out.png
+ */
+const createTile = (source, dest, canvassize, tilesize, x, y) => {
+  return new Promise((resolve, reject) => {
+    shell.mkdir('-p', `${dest}/${y}`)
+    console.log(
+      ` > gm convert ${source} -resize ${canvassize}x${canvassize} -crop ${tilesize}x${tilesize}+${x *
+        tilesize}+${y * tilesize}`
+    )
+    gm(source)
+      .out('-resize', `${canvassize}x${canvassize}`)
+      .out('-crop', `${tilesize}x${tilesize}+${x * tilesize}+${y * tilesize}`)
+      .write(`${dest}/${y}/${x}.png`, err => (err ? reject(err) : resolve()))
+  })
 }
 
 /**
  * Run app
  */
-app(process.argv)
+app(process.argv, 256)
